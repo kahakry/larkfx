42a0d9895b2e8005d0e320f9f737b8d612a8d9eb
kahakry <kahakry@gmail.com>
2026-01-22 02:16:18 +0800

    feat：完成项目结构

---
+++ larkfx-core/src/main/java/com/kahakry/larkfx/core/controls/LxButton.java
@@ -0,0 +1,12 @@
+package com.kahakry.larkfx.core.controls;
+
+import javafx.scene.control.Button;
+
+/**
+ * 按钮
+ *
+ * @author kahakry
+ * @date 2026-01-22 00:51:57
+ */
+public class LxButton extends Button {
+}
---
+++ larkfx-core/src/main/java/com/kahakry/larkfx/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *
+ *
+ * @author kahakry
+ * @date 2026-01-22 00:51:06
+ */
+package com.kahakry.larkfx.core;
